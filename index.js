0c3abce56b9c868c1b814c59d663f0f03eb04cdd
srinidhi <srinidhivk1989@gmail.com>
2018-02-05 14:56:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+
+//set up express app
+const app=express();
+
+//Connect to mongodb
+mongoose.connection.openUri('mongodb://localhost/ninjago');﻿
+console.log('hi');
+mongoose.Promise=global.Promise;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+//initialize routes
+app.use('/api',require('./routes/api'));
+
+//error handling middleware
+app.use(function(err,req,res,next){
+  //console.log(err);
+  res.send({error: err._message});
+});
+
+//listen for requests
+app.listen(4000,function(){
+console.log('now listening for requests');
+});
